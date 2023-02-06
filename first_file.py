6b82a83af06fda64a09045f5a50db6bb6ba892d8
Konstantin Tikhmenev <newemail@yandex.ru>
2023-02-06 15:23:17 +0300

    "Поколение Python": курс для продвинутых

--- first_file.py
+++ first_file.py
@@ -1,3 +1,9 @@
-print('Hello World')
-import math
-print(math.pi)
+m = float(input())
+r = float(input())
+imt = m / ( r ** 2)
+if 18.5 <= imt <= 25:
+    print('Оптимальная масса')
+elif imt < 18.5:
+    print('Недостаточная масса')
+else:
+    print('Избыточная масса')
